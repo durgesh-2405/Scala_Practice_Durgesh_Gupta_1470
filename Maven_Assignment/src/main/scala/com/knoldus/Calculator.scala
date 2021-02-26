77daf761eebc0f2799fcd4f5114bcc117989dff3
Durgesh <durgesh.gupta@knoldus.com>
2021-02-26 17:05:40 +0530

    Calculator Assignment Using Maven By Durgesh Gupta

---
+++ Maven_Assignment/src/main/scala/com/knoldus/Calculator.scala
@@ -0,0 +1,72 @@
+//Calculator-By Durgesh Gupta
+package com.knoldus
+
+class Calculator {
+  def addition(addend1:Int, addend2:Int):Int = addend1 + addend2
+
+  def subtraction(minuend:Int, subtrahend:Int):Int = minuend - subtrahend
+
+  def multiplication(multiplier:Int, multiplicand:Int):Long = multiplier * multiplicand
+
+  //This method is for Division if the denominator/divisor is zero it will print the message and also return the value zero
+  def division(dividend:Int , divisor:Int):Double = {
+    var result = 0
+    if(divisor != 0){
+      result = dividend/divisor
+    }
+    else{
+      print("Divisor can't be zero")
+    }
+    result
+  }
+
+
+  //In this method there is a loop through which the the value multiply itself until the loop's iteration does npt equal to power
+  def power(value:Int , power:Int):Long = {
+    var result = 1
+    var temporary_variable = power
+    while(temporary_variable > 0){
+      result = result*value
+      temporary_variable = temporary_variable - 1
+    }
+    result
+  }
+
+  //It will return always positive value.
+  def absolute(value: Int):Int = Math.abs(value)
+
+  // Method to return modulus of two numbers
+
+  def modulus(number_1: Int , number_2: Int): Int = number_1 % number_2
+
+  //Return maximum value from the given two
+  def maximum(number_1:Int,number_2:Int):Int={
+    var result = 0
+
+    if(number_1 > number_2){
+      result = number_1
+    }
+    else{
+      result = number_2
+    }
+
+    result
+  }
+
+
+
+  //Return minimum value from the given two
+  def minimum(number_1:Int,number_2:Int):Int={
+    var result = 0
+
+    if(number_1 < number_2){
+      result = number_1
+    }
+    else{
+      result = number_2
+    }
+
+    result
+  }
+
+}
---
+++ Maven_Assignment/src/test/scala/com/knoldus/CalculatorTest.scala
@@ -0,0 +1,64 @@
+//Calculator-By Durgesh Gupta
+package com.knoldus
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class CalculatorTest extends AnyFlatSpec{
+  val number_1=20
+  val number_2=30
+  val number_3=40
+  val number_4=5
+  val number_5=2
+
+  "The addition of 20 and 30" should "give 50" in {
+    val sum = new Calculator()
+    val result= sum.addition(number_1,number_2)
+    assert(result==50)
+  }
+
+  "The subtraction of 20 and 30" should "give -30" in {
+    val sub = new Calculator()
+    val result= sub.subtraction(number_1,number_2)
+    assert(result== -10)
+  }
+
+  "The multiplication of 20 and 5" should "give 100" in {
+    val mul = new Calculator()
+    val result= mul.multiplication(number_1,number_4)
+    assert(result==100)
+  }
+  "The division of 20 and 5" should "give 4" in {
+    val div = new Calculator()
+    val result= div.division(number_1,number_4)
+    assert(result== 4)
+  }
+  "5 raise to power 2" should "give 25" in {
+    val pow = new Calculator()
+    val result= pow.power(number_4,number_5)
+    assert(result==25)
+  }
+  "Absolute value of 20" should "give 20" in {
+    val abs = new Calculator()
+    val result= abs.absolute(number_1)
+    assert(result==20)
+  }
+
+  "The modulus of 30 and 20" should "give 10" in {
+    val mod = new Calculator()
+    val result= mod.modulus(number_2,number_1)
+    assert(result==10)
+  }
+
+  "The Maximum of 40 and 30" should "give 40" in {
+    val max = new Calculator()
+    val result= max.maximum(number_3,number_2)
+    assert(result==40)
+  }
+  "The Minimum of 5 and 2" should "give 2" in {
+    val min = new Calculator()
+    val result= min.minimum(number_4,number_5)
+    assert(result==2)
+  }
+
+
+}
